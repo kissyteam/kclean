30c02e98375d9718b182ea9a1f79904c2b731507
淘杰 <taojie.hjp@taobao.com>
2014-09-12 12:11:06 +0800

    去除kclean之后生成的多余代码

--- test/clean_kissy.js
+++ test/clean_kissy.js
@@ -1,32 +1,17 @@
-KISSY.add("bee-demo/index", ["node"], function(S ,require, exports, module) {
+KISSY.add("bee-demo/index", ["node","./mods/header"], function(S ,require, exports, module) {
 var node = require("node");
-var beeDemoModsHeader, beeDemoModsArticle, beeDemoIndex;
+var modsHeader = require("./mods/header");
+var beeDemoModsHeader, beeDemoIndex;
 beeDemoModsHeader = function (exports) {
-  exports = {};
-  var e = node.all;
+  var e = node;
   exports = {
     init: function () {
-      S.log('header init'), e('header').html('this is header');
-    }
-  };
-  return exports;
-}();
-beeDemoModsArticle = function (exports) {
-  exports = {};
-  var i = node.all;
-  exports = {
-    init: function () {
-      S.log('article init'), i('article').html('this is article');
     }
   };
   return exports;
 }();
 beeDemoIndex = function (exports) {
   exports = {};
-  var e = beeDemoModsHeader;
-  e.init();
-  var r = beeDemoModsArticle;
-  r.init();
   exports.a = 123;
   return exports;
 }();
--- test/kissy.js
+++ test/kissy.js
@@ -1,7 +1,12 @@
-KISSY.add('bee-demo/index',["./mods/header","./mods/article"],function(S ,require, exports, module) {var e=require("./mods/header");
-e.init();var r=require("./mods/article");
-r.init();
+KISSY.add('bee-demo/index', ["./mods/header", "./mods/article"],function(S, require, exports, module) {
+	require("./mods/header");
+    exports.a = 123;
+});
 
-exports.a=123;
-
-});KISSY.add('bee-demo/mods/header',["node"],function(S ,require, exports, module) {var e=require("node").all;module.exports={init:function(){S.log("header init"),e("header").html("this is header")}};});KISSY.add('bee-demo/mods/article',["node"],function(S ,require, exports, module) {var i=require("node").all;module.exports={init:function(){S.log("article init"),i("article").html("this is article")}};});
+KISSY.add('bee-demo/mods/header', ["node"], function(S, require, exports, module) {
+	var e = require("node");
+	module.exports = {
+		init: function() {
+		}
+	};
+});
