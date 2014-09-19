3b0d68300e3cd7dc5e9953cf5368591a262c5512
淘杰 <taojie.hjp@taobao.com>
2014-09-19 11:49:47 +0800

    fix some bugs and upgrade a new version

--- test/test_sea.js
+++ test/test_sea.js
@@ -1,9 +1,9 @@
 var kclean = require('kclean'),
     fs = require('fs');
 
-var code = fs.readFileSync("sea.js").toString(),
+var code = fs.readFileSync("modulex.js").toString(),
     cleanedCode = kclean.clean(code,{
-         outputModule:"sys/main"
+         outputModule:"anim/base"
     });
 
 fs.writeFileSync("clean_sea.js",cleanedCode);
